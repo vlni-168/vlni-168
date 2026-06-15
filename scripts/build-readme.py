0284a944e024d1ac39148aacefebc6b2dcb663af
Valentin <valentin.ahlfeld@outlook.de>
2026-06-15 22:25:40 +0200

    Update build-readme.py

--- scripts/build-readme.py
+++ scripts/build-readme.py
@@ -12,7 +12,7 @@ for level in range(5, 0, -1):
         icons.append(
             f'<img src="https://cdn.simpleicons.org/{slug}" '
             f'width="32" height="32" '
-            f'style="display: inline-block; background: #E8E8E8; '
+            f'style="display: inline-block; background: #ffffff; '
             f'border-radius: 4px; padding: 4px; vertical-align: middle;" />'
         )
     # Wrap after every 5 icons, spaces between

d2b50f0980a754d5778a792082cef0a98026a8f6
Valentin <valentin.ahlfeld@outlook.de>
2026-06-15 10:53:30 +0200

    Update build-readme.sh

--- scripts/build-readme.sh
+++ scripts/build-readme.sh
@@ -9,16 +9,16 @@ if [ ! -f "$TEMPLATE" ] || [ ! -f "$JSON_FILE" ]; then
     exit 1
 fi
 
-TABLE_CONTENT="| Level | Skills |\n| ----- | ------ |\n"
+TABLE_CONTENT=$(printf "| Level | Skills |\n| ----- | ------ |\n")
 
 for level in {5..1}; do
     skills=$(jq -r --arg lvl "$level" '.[$lvl] | if . then .[] else empty end' "$JSON_FILE")
-    
+
     ROW_ICONS=""
     for skill in $skills; do
         ROW_ICONS="$ROW_ICONS <img src=\"https://cdn.simpleicons.org/$skill\" style=\"background: #f0f0f0; border-radius: 4px; padding: 4px;\" height=\"32\" />"
     done
-    TABLE_CONTENT="${TABLE_CONTENT}| $level |$ROW_ICONS |\n"
+    TABLE_CONTENT="${TABLE_CONTENT}$(printf "| $level |$ROW_ICONS |\n")"
 done
 
 if grep -q "{{SKILLS_TABLE}}" "$TEMPLATE"; then
@@ -26,6 +26,7 @@ if grep -q "{{SKILLS_TABLE}}" "$TEMPLATE"; then
     perl -0777 -pe 's/\{\{SKILLS_TABLE\}\}/$ENV{TABLE_CONTENT}/g' "$TEMPLATE" > "$OUTPUT"
 else
     cat "$TEMPLATE" > "$OUTPUT"
-    echo -e "\n## Skills\n$TABLE_CONTENT" >> "$OUTPUT"
+    printf "\n## Skills\n$TABLE_CONTENT" >> "$OUTPUT"
 fi
-echo "README.md generated sucessfully!"
+
+echo "README.md generated successfully!"
